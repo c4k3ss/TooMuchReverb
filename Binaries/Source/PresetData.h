9c89e6e8a56afce1b061fa0f3c41e715b91596b8
c4k3ss <iustiniorgus@gmail.com>
2025-08-24 18:19:05 +0300

    Windows Build

--- Binaries/Source/PresetData.h
+++ Binaries/Source/PresetData.h
@@ -6,7 +6,7 @@
 namespace PresetData
 {
     extern const char*  externalFiles;
-    const int           externalFilesSize = 90;
+    const int           externalFilesSize = 59007;
 
     extern const char*  images;
     const int           imagesSize = 540298;
@@ -18,7 +18,7 @@ namespace PresetData
     const int           midiFilesSize = 28;
 
     extern const char*  preset;
-    const int           presetSize = 5297;
+    const int           presetSize = 5351;
 
     extern const char*  samplemaps;
     const int           samplemapsSize = 28;
